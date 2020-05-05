7c760d66a84e4142274e4def013a4beafcd076c4
anchliyatushar <anchliyatushar@gmail.com>
2020-05-05 11:06:38 +0530

    android x enabled | fix future in app | added dependency gradle build

--- lib/main.dart
+++ lib/main.dart
@@ -12,73 +12,67 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  AuthService authService= AuthService();
-  final formalkey =  GlobalKey<FormState>();
+  AuthService authService = AuthService();
+  final formalkey = GlobalKey<FormState>();
   String error = '';
   String email = '';
   String pass = '';
-TextEditingController myContro = TextEditingController();
+  TextEditingController myContro = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Form(
-          key : formalkey,
+          key: formalkey,
           child: Column(
             children: <Widget>[
-            TextField(
-              decoration: InputDecoration(icon: Icon(Icons.email)),
-              onChanged:(val) {
-                setState(() {
-                email = val;
-                });
+              TextField(
+                decoration: InputDecoration(icon: Icon(Icons.email)),
+                onChanged: (val) {
+                  setState(() {
+                    email = val;
+                  });
                 },
-            ),
-            SizedBox(height: 5,),
+              ),
+              SizedBox(
+                height: 5,
+              ),
               TextField(
                 decoration: InputDecoration(icon: Icon(Icons.remove_red_eye)),
-                onChanged:(val) {
+                onChanged: (val) {
                   setState(() {
                     pass = val;
                   });
                 },
               ),
-              ActionChip(label: Text("Sing In"),backgroundColor: Colors.blue, onPressed: () {
-                  AuthService my  = AuthService(name: email,pass:pass ) ;
-                error = my.signIn();
-                ShowTost(error);
-              })
-        ],
-        ),
-
+              ActionChip(
+                  label: Text("Sing In"),
+                  backgroundColor: Colors.blue,
+                  onPressed: () async {
+                    AuthService my = AuthService(name: email, pass: pass);
+                    error = await my.signIn();
+                    ShowTost(error);
+                  })
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
- ShowTost(String error) {
-
-}
+ShowTost(String error) {}
